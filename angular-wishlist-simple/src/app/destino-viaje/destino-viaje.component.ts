33e1b238e729d15ab82a71a33ac4aa0703c4f9cf
prov_jhern <prov_jhern@eafit.edu.co>
2020-09-22 18:31:25 -0500

    Componentes y Controladores

--- angular-wishlist-simple/src/app/destino-viaje/destino-viaje.component.ts
+++ angular-wishlist-simple/src/app/destino-viaje/destino-viaje.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, EventEmitter, Output } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
 
 @Component({
@@ -8,11 +8,19 @@ import { DestinoViaje } from '../models/destino-viaje.model';
 })
 export class DestinoViajeComponent implements OnInit {
   @Input() destino: DestinoViaje;
-  @HostBinding('attr.class') cssClass = 'col-md-4'
+  @HostBinding('attr.class') cssClass = 'col-md-4';
+  @Output() clicked: EventEmitter<DestinoViaje>;
 
-  constructor() { }
+  constructor() {
+    this.clicked = new EventEmitter();
+  }
 
   ngOnInit(): void {
+  }
+
+  ir(){
+    this.clicked.emit(this.destino);
+    return false;
   }
 
 }
--- angular-wishlist-simple/src/app/models/destino-viaje.model.ts
+++ angular-wishlist-simple/src/app/models/destino-viaje.model.ts
@@ -1,9 +1,13 @@
 export class DestinoViaje {
-    nombre: string;
-    imagenUrl: string;
+    private selected: boolean;
 
-    constructor(n:string, u:string){
-        this.nombre = n;
-        this.imagenUrl = u;
+    constructor(public nombre:string, imagenUrl:string) { }
+
+    isSelected(): boolean {
+        return this.selected;
+    }
+
+    setSelected(s: boolean) {
+        this.selected = s;
     }
 }
